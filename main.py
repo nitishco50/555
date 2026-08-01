d850f094b8311437089450fccb0bef0afcc74b5f
nitishco50 <nkuamr519@gmail.com>
2026-08-01 09:58:15 +0530

    Refactor download endpoint to use get_video function

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from downloader import download_instagram_video
+
+from downloader import get_video
 
 app = FastAPI(title="Instagram Downloader API")
 
-# Allow frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # Production me apna domain dena
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,11 +21,11 @@ class DownloadRequest(BaseModel):
 @app.get("/")
 def home():
     return {
-        "success": True,
-        "message": "Instagram Downloader API is running."
+        "status": "running",
+        "message": "Instagram Downloader API"
     }
 
 
 @app.post("/download")
 def download(data: DownloadRequest):
-    return download_instagram_video(data.url)
+    return get_video(data.url)
